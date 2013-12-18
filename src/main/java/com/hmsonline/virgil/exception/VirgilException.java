9399ddc8f2b6960aa2e7dc353c557a7f6b16c2ff
Brian O'Neill <bone@alumni.brown.edu>
2013-12-17 23:08:01 -0500

    Updates to rename to memnon.

--- src/main/java/com/hmsonline/virgil/exception/VirgilException.java
+++
@@ -1,9 +0,0 @@
-package com.hmsonline.virgil.exception;
-
-public class VirgilException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public VirgilException(String message){
-        super(message);
-    }   
-}
